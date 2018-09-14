bd8293cc6713643f37d2ceb7ee830addec4eb25c
Sean Budhram <sbudhram@carlisle-etcetera.com>
2018-09-14 16:06:10 -0400

    added jquery

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
-const game = new Game();
+// const game = new Game();
+/*
+eventlistener for start game needed
+*/
 
-//eventlistener for start game needed
+$('#btn__reset').on('click', () => console.log('start'));
+
 function resetDisplay() {};
 function markButton() {};
 // evernt listener for keyboard button so that clicking a button calls the
